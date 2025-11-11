d19fab6503364d4fb04d7e83bc91c225d7097120
Kingsley <kifeanyi216@gmail.com>
2025-11-11 16:20:54 +0100

    Product Section

---
+++ src/assets/index.js
@@ -0,0 +1,21 @@
+import hero from "./hero.jpg";
+import product from "./product.jpg";
+import product1 from "./product1.jpg";
+import product2 from "./product2.jpg";
+import product3 from "./product3.jpg";
+import product4 from "./product4.jpg";
+import product5 from "./product5.jpg";
+import product6 from "./product6.jpg";
+import product7 from "./product7.jpg";
+
+export {
+  hero,
+  product,
+  product1,
+  product2,
+  product3,
+  product4,
+  product5,
+  product6,
+  product7,
+};
