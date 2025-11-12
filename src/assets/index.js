f0c4a8aca357d72b3c18ad54ee85980d3f9310d6
Kingsley <kifeanyi216@gmail.com>
2025-11-12 15:19:51 +0100

    Categories Section

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,6 +7,12 @@ import product4 from "./product4.jpg";
 import product5 from "./product5.jpg";
 import product6 from "./product6.jpg";
 import product7 from "./product7.jpg";
+import product8 from "./product8.jpg";
+import product9 from "./product9.jpg";
+import product10 from "./product10.jpg";
+import product11 from "./product11.jpg";
+import product12 from "./product12.jpg";
+import product13 from "./product13.jpg";
 
 export {
   hero,
@@ -18,4 +24,10 @@ export {
   product5,
   product6,
   product7,
+  product8,
+  product9,
+  product10,
+  product11,
+  product12,
+  product13,
 };
